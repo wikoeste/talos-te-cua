7910cd45e4a22e913989adb8e332cc159a6e3d24
wikoeste <wikoeste@cisco.com>
2025-04-14 10:08:58 -0400

    added option to read a xlsx file exported from ace and inject the url (entry column) to cua

--- README.md
+++ README.md
@@ -3,8 +3,11 @@
 The purpose is to take ACE URL data and pipe it into CUA to possibel assist in auto resolving cases.
 
 # Release Notes
+0.2\
+take the url list and parse to remove hxxps or hxxp from the submissions
 
 0.1\
 initial release uses python requests\
 tests connectivity and authentication to armory\
-take a list of urls or single url and posts to armory/cua for telemetry injection
+user inputs a xlsx file from ACE to extract all the entries (urls) and inject to ACE
+take a hard coded list of urls or single url and posts to armory/cua for telemetry injection
--- cua/main.py
+++ cua/main.py
@@ -1,8 +1,11 @@
+import os
+
 from cua.common import settings
 settings.init()
 from cua_utils import publisher
 from mrt.armory import Armory
 import requests,json
+import pandas as pd
 
 
 # Test API access
@@ -10,9 +13,9 @@ def testapi():
     res = requests.get("https://armory-sl.sco.cisco.com/whoami",headers={"x-armory-key":settings.cuakey},verify=False)
     if res.status_code == 200:
         rj  = res.json()
-        print("CUA API Test Results:\n", json.dumps(rj, indent=2))
+        print("CUA API Test Successful!!!!\n", json.dumps(rj, indent=2))
     else:
-        print("\nHTTP ERR:{}".format(res.status_code))
+        print("CUA API Test FAIL!!!!\n HTTP ERR:{}".format(res.status_code))
 
 # POST / Inject
 def inject(urls):
@@ -25,10 +28,55 @@ def inject(urls):
     else:
         print("\nHTTP ERR:{}".format(res.status_code))
 
+# Read CSV column for urls exported from ACE and inject to cua
+def readxls():
+    filePath    = None
+    fnames      = []
+    count       = 1
+    os.chdir(os.path.expanduser("~/Downloads"))
+    cwd         = os.getcwd()
+    print("Listing the Files from your Download  directory to select: ")
+    #Print the list of files
+    for item in os.listdir():
+        if os.path.isfile(item) and not item.startswith('.') and item.endswith('.xlsx'):
+            with open(item, "rb") as f:
+                fbytes = f.read()  # read entire file as bytes
+                print(str(count) + ". " + item)
+                fnames.append(item)
+                count += 1
+    pick     = input("Select the File for CUA Injection from the list above : ")
+    choice   = int(pick)
+    choice   -= 1
+    filePath = cwd + "/" + fnames[choice]
+    exclfile = pd.ExcelFile(filePath)
+    try:
+        df       = exclfile.parse()
+        colname  = 'Entry'
+        coldata  = df[colname]
+        print("Column Name: Entry\n",coldata)
+        res = df[colname].tolist()
+        return res
+        #Print by index of the column
+        #col      = 2
+        #colindex = df.iloc[:, col]
+        #print("Column Index\n", colindex)
+    except FileNotFoundError:
+        print(f"Error: File not found at '{filePath}'")
+        return []
+    except KeyError:
+        print(f"Error: Column '{coldata}' not found in the sheet")
+        return []
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+        return []
+
+####MAIN######
 def main():
     urls = ["https://1234computers.com","http://examplemalwaredomain.com","https://phish.opendnstest.com"]
     #print(f"CUA API KEY {settings.cuakey}")
     testapi()
+    #inject(urls)
+    urls = readxls()
     inject(urls)
     #armory = Armory()
     #armory.inject(urls)
